f4cfa28272cad7b3786f151f8c55cad4dae24ff0
aakashvishu120 <aakashvishu120@gmail.com>
2025-03-26 17:06:14 +0530

    expoloring GET POST DELETE method of HTTP

---
+++ src/Readme.md
@@ -0,0 +1,27 @@
+run command  :npm run start
+if node_modules was deleted then run command : npm  install
+
+
+
+Notes : 
+- Create a respositiry
+- intialise the repository
+- node_modules , package.json, package-lock.json
+- install express
+- create a server
+- listen to a port 7777
+- write request handlers for /test, /hello
+- install nodemon and update scripts inside package.json
+- what are dependencies
+- what is the use of -g while npm insatll
+- difference between caret and tilde(^ vs ~)
+
+- Initialize git
+- gitignore
+- create a remote repo on github
+- Push all code to remote origin
+- Play with routes and route extension ex. /hello, /, hello/2, /xyz
+- Order of the routes matter a lot 
+- Install a Postamn app and make workspace/collection and test api call 
+- we can make same APIS on different HTTP method
+- write logic to handle GET, POST , PATCH, DELETE Api calls and test then on postman.
--- src/app.js
+++ src/app.js
@@ -1,19 +1,33 @@
 const express = require("express");
 const app= express();
 
+app.get("/user", (req,res)=>{
+    res.send({firstName: "Aakash" , LastName : "Kumar"});
+})
 
-app.use("/hello",(req,res)=>{
-    res.send("hello hello hello");
-});
+app.post("/user", (req,res)=>{
+    console.log("save data to the database");
+    res.send("Data saved successfully");
+})
 
-app.use("/test",(req,res)=>{
-    res.send("hello from the test");
-});
+app.delete("/user", (req,res)=>{
+    res.send("Data delete successfully");
+})
 
-app.use("/",(req,res)=>{
-    res.send("hello from the dashboard");
-});
+//use match all the http method APi call to /hello
+// app.use("/hello",(req,res)=>{
+//     res.send("hello hello hello");
+// });
 
-app.listen(3000, ()=>{
+// app.use("/test",(req,res)=>{
+//     res.send("hello from the test");
+// });
+
+// //sequences of route matter, place this route always at the end else it will override other route
+// app.use("/",(req,res)=>{
+//     res.send("hello from the dashboard");
+// });
+
+app.listen(7777, ()=>{
     console.log("server is listening");
 });
